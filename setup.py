841324d86b3f1478af47d46104bb96d29da9bd5f
JelicaB <jelica.bornath@gmail.com>
2021-09-01 17:18:28 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of attributes and trends in songs on the Billboard Hot 100 chart',
+    author='Jelica Bornath',
+    license='MIT',
+)
